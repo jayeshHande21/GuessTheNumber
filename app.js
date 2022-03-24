663bf9a07e180b28da5de37c9aa7f375f3bac8fd
Jayu <jayeshhande00@gmail.com>
2022-03-24 19:14:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const check = document.querySelector(".check");
+
+const message = document.querySelector(".message");
+const show = document.querySelector(".number");
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+const showScore = document.querySelector(".score");
+
+let score = 20;
+show.textContent = secretNumber;
+
+function checkScore() {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    message.textContent = "NO INPUT⛔⛔";
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      message.textContent = "too less 📉";
+      score--;
+      showScore.textContent = score;
+    } else {
+    }
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      message.textContent = "too more ☝☝";
+      score--;
+
+      showScore.textContent = score;
+    } else {
+      message.textContent = "YOU LOST 😢😢";
+    }
+  } else if (guess === secretNumber) {
+    message.textContent = "Yor Are Correct🥳🥳";
+    score++;
+    showScore.textContent = score;
+  }
+}
+
+check.addEventListener("click", checkScore);
