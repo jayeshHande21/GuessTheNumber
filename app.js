2f47c9b359d4e7ffbb6a3b1fef57c3c89b69469a
Jayu <jayeshhande00@gmail.com>
2022-03-24 23:02:27 +0530

    final commit

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 const check = document.querySelector(".check");
 
-const message = document.querySelector(".message");
-const show = document.querySelector(".number");
-
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-
+let message = document.querySelector(".message");
+let show = document.querySelector(".number");
 const showScore = document.querySelector(".score");
+let again = document.querySelector(".again");
+let showHighScore = document.querySelector(".highscore");
 
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+show.textContent = secretNumber;
 let score = 20;
-
+let HighScore = 0;
 function checkScore() {
-  const guess = Number(document.querySelector(".guess").value);
+  let guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     message.textContent = "NO INPUT⛔⛔";
@@ -22,6 +23,7 @@ function checkScore() {
       score--;
       showScore.textContent = score;
     } else {
+      body.style.backgroundColor = "red";
       message.textContent = "YOU LOST THE SAME 😢😢";
     }
   } else if (guess > secretNumber) {
@@ -31,13 +33,31 @@ function checkScore() {
 
       showScore.textContent = score;
     } else {
+      body.style.backgroundColor = "red";
       message.textContent = "YOU LOST THE SAME 😢😢";
     }
   } else if (guess === secretNumber) {
     message.textContent = "Yor Are Correct🥳🥳";
-    score++;
-    showScore.textContent = score;
+    show.textContent = secretNumber;
+    // score++;
+    body.style.backgroundColor = "green";
+    show.style.width = "30rem";
+    if (score > HighScore) {
+      HighScore = score;
+      showHighScore.textContent = HighScore;
+    }
   }
 }
 
+again.addEventListener("click", function () {
+  console.log("refresh done");
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  message.textContent = "Start Guessing";
+  show.textContent = "?";
+  body.style.backgroundColor = "black";
+  show.style.width = "15rem";
+  document.querySelector(".guess").value = "";
+  showScore.textContent = score;
+});
 check.addEventListener("click", checkScore);
